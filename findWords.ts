e7ef847f7cbed77e7b436d6616cd38d245025e02
Diego Monroy F <diego.monroy.f@gmail.com>
2024-03-23 12:51:41 -0500

    Adding functionality to isInInputString().

--- findWords.ts
+++ findWords.ts
@@ -1,11 +1,25 @@
 // WW Coding Challenge -- findWords
 
-function isInInputString(characters: Map<string, number>, word: string): boolean {
-  // TODO
+function isInInputString(
+  characters: Map<string, number>,
+  word: string
+): boolean {
+  // Check if the specified word can be made using the characters in the Map.
+  // We iterate through each character in the word and see if it's included
+  // in the Map.
   for (let i: number = 0; i < word.length; i++) {
-
+    let c: number | undefined = characters.get(word[i]);
+    // If it's not included, then we are "missing" characters; thus it's not
+    // possible to make this word with these characters.
+    if (c === undefined) return false;
+    // If it's down to one occurrence for this character, simply delete the entry.
+    if (c === 1) characters.delete(word[i]);
+    // Otherwise, decrement counter by 1.
+    characters.set(word[i], c - 1);
   }
-  return false;
+  // If the whole word has been inspected and the characters Map was not
+  // exhausted, then the word CAN be made using the characters map.
+  return true;
 }
 
 function findWords(inputString: string, dictionary: string[]): string[] {
@@ -25,9 +39,11 @@ function findWords(inputString: string, dictionary: string[]): string[] {
   let result: string[] = [];
 
   // Now that we have the map of characters and occurrences, use that data
-  // structure to see if each word in the dictionary is included in the inputString.
+  // structure to see if each word in the dictionary is included in the
+  // inputString.
   dictionary.forEach((word: string) => {
-    // We make a shallow copy of the original map, so we can reuse it multiple times.
+    // We make a shallow copy of the original map, so we can reuse it
+    // multiple times.
     if (isInInputString(new Map<string, number>(characters), word)) {
       result.push(word);
     }
